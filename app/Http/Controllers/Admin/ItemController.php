91c1a8f86e4cc7c623265939a31fd82f61aecc92
Ramaaditya <wayanramaaditya@gmail.com>
2024-07-15 15:56:35 +0800

    penambahan file create dan index week5-ses9 menit 1:12:31

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Item;
+use App\Models\Brand;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use App\Http\Controllers\Controller;
@@ -48,7 +50,10 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        $brands = Brand::all();
+        $types = Type::all();
+
+        return view('admin.items.create', compact('brands', 'types'));
     }
 
     /**
